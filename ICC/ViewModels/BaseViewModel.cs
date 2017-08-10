bc44a04ed70b234221d65adc7cadb03210ff0db7
JuneCho23 <junecho.ucb@gmail.com>
2017-08-10 09:17:24 -0700

    BaseViewModel Completed

---
+++ ICC/ViewModels/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ICC.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+		public BaseViewModel()
+		{
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void SetProperty<T>(ref T backingStore, T value, [CallerMemberName]string propertyName = "", Action onChanged = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(backingStore, value))
+				return;
+			backingStore = value;
+			onChanged?.Invoke();
+			OnPropertyChanged(propertyName);
+		}
+
+		protected virtual void OnPropertyChanged([CallerMemberName] //This is an Attribute, always in Square Brackets
+                                                 string propertyName=null)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+    }
+}

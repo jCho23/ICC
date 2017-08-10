bcae30e70bfca29915afe122b7a5b9406c4686a4
JuneCho23 <junecho.ucb@gmail.com>
2017-08-10 09:15:52 -0700

    BasePage Created

---
+++ ICC/Pages/BasePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ICC
+{
+	public class BaseContentPage<T> : ContentPage where T : BaseViewModel, new()
+	{
+		T _viewModel;
+
+		public BaseContentPage()
+		{
+			this.BindingContext = MyViewModel;
+		}
+
+		protected T MyViewModel
+		{
+			get
+			{
+				return _viewModel ?? (_viewModel = new T());
+			}
+		}
+	}
+}

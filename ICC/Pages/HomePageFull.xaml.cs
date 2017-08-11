92757f7756dd8434abb5dc556c2dba36f7858b81
JuneCho23 <junecho.ucb@gmail.com>
2017-08-10 19:09:15 -0700

    ObservableStringList

--- ICC/Pages/HomePageFull.xaml.cs
+++ ICC/Pages/HomePageFull.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using ICC.Data;
 
 namespace ICC.Pages
 {
@@ -10,6 +11,8 @@ namespace ICC.Pages
         public HomePageFull()
         {
             InitializeComponent();
+
+            listView.ItemsSource = VideoData.ObservableStringList;
         }
     }
 }

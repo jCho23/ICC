895ffcfaa11d9e3bba2cb76546e61f4298c5bd76
JuneCho23 <junecho.ucb@gmail.com>
2017-08-10 18:53:45 -0700

    Created Video Data

---
+++ ICC/Data/VideoData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace ICC.Data
+{
+    public class VideoData : ContentPage
+    {
+        public class VideoData
+        {
+            public static ObservableCollection<string> ObeservableStringList = new ObservableCollection<string>
+            {
+                "1","2","3"
+            };
+
+        };
+    }
+}
+

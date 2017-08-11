e48b9bb3a4acdbd2a29d257da5b62895c64ab32e
JuneCho23 <junecho.ucb@gmail.com>
2017-08-10 18:56:37 -0700

    videoName

--- ICC/Data/VideoData.cs
+++ ICC/Data/VideoData.cs
@@ -6,7 +6,14 @@ namespace ICC.Data
 {
     public class VideoData : ContentPage
     {
-        public class VideoData
+        public class VideoName
+        {
+            public string Name { get; set; }
+
+        }
+
+
+        public class VideoDatas
         {
             public static ObservableCollection<string> ObeservableStringList = new ObservableCollection<string>
             {

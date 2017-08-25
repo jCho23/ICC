e2fc085271fcdbd78362a925d1e325b7dc00609f
JuneCho23 <junecho.ucb@gmail.com>
2017-08-24 17:39:45 -0700

    VideosListLayout C# CB

--- ICC/Views/VideosListLayout.xaml.cs
+++ ICC/Views/VideosListLayout.xaml.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
 using Xamarin.Forms;
 
 namespace ICC.Views
 {
     public partial class VideosListLayout : ContentPage
     {
+        private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
+
         public VideosListLayout()
         {
             InitializeComponent();
 
-			//listView.ItemsSource = VideoData.VideoDatas.ObservableStringList;
+			CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+			{
+				Device.BeginInvokeOnMainThread(() =>
+				{
+					ProgressBar.Progress = e.Progress;
+					Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+				});
+			};
+
 		}
+
+		protected override void OnAppearing()
+		{
+			videoView.Source = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+		}
+
+		void PlayClicked(object sender, System.EventArgs e)
+		{
+			PlaybackController.Play();
+		}
+
+		void PauseClicked(object sender, System.EventArgs e)
+		{
+			PlaybackController.Pause();
+		}
+
+		void StopClicked(object sender, System.EventArgs e)
+		{
+			PlaybackController.Stop();
+		}
+
     }
 }
